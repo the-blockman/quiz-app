1e81d6aa623583c62a47f9f6a236f13905298620
the-blockman <105639638+the-blockman@users.noreply.github.com>
2026-03-10 07:53:37 +0100

    feat: update fetchQuizQuestions to include category parameter for API requests

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
-export const fetchQuizQuestions = async (amount, difficulty) => {
+export const fetchQuizQuestions = async (amount, difficulty, category) => {
   try {
     const response = await fetch(
-      `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`,
+      `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&category=${category}&type=multiple`,
     );
 
     const data = await response.json();
